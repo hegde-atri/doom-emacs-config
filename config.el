2cfc08d6b817f75b4b8923e5d5b0b173ced11a3c
hegde-atri <iamatrihegde@gmail.com>
2022-04-10 17:27:21 +0100

    p1

--- config.el
+++ config.el
@@ -31,9 +31,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
+;; This dEtermines The Style Of Line Numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type "relative")
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -44,11 +44,13 @@
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
-;; - `map!' for binding new keys
+;; - `Map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; they are implemented.#
+;; Setting projectiles directory
+(setq projectile-project-search-path '("~/repos/" "/home/mizuuu/repos/"))
