5f8ada342c7d0a039e4a8d652ff2f96b25e4992c
hegde-atri <iamatrihegde@gmail.com>
2022-04-23 09:32:55 +0100

    added colemak binds package

--- config.el
+++ config.el
@@ -65,7 +65,15 @@
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
+
+(use-package! evil-colemak-basics
+  :after evil
+  :init
+  :config
+  (global-evil-colemak-basics-mode)
+  )
+
+
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
