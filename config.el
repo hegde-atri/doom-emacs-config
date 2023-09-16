1b33433c6f2d5e25cb81aae24bf0dc5d4008030f
Atri Hegde <iamatrihegde@outlook.com>
2023-09-16 19:48:09 +0100

    feat: projectile recursive project discovery

--- config.el
+++ config.el
@@ -268,7 +268,24 @@
   (setq lsp-ui-doc-show-with-cursor nil
         lsp-ui-doc-show-with-mouse t))
 
-(setq projectile-project-search-path '("~/repos/"))
+(setq projectile-project-search-path '("~/repos"))
+;; Fix non-recursive discovery
+(defun projectile-discover-projects-in-directory (directory)
+  "Discover any projects in DIRECTORY and add them to the projectile cache.
+This function is not recursive and only adds projects with roots
+at the top level of DIRECTORY."
+  (interactive
+   (list (read-directory-name "Starting directory: ")))
+  (let ((subdirs (directory-files directory t)))
+    (mapcar
+     (lambda (dir)
+       (when (and (file-directory-p dir)
+                  (not (member (file-name-nondirectory dir) '(".." "."))))
+         (let ((default-directory dir)
+               (projectile-cached-project-root dir))
+           (when (projectile-project-p)
+             (projectile-add-known-project (projectile-project-root))))))
+     subdirs)))
 
 (use-package! evil-nerd-commenter
   :init (evilnc-default-hotkeys))
