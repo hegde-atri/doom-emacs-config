d6b4f91e5ee262bf33cb0fba2fef989f116c6cb6
Atri Hegde <iamatrihegde@outlook.com>
2023-12-06 15:28:14 +0000

    snippets + copilot

--- config.el
+++ config.el
@@ -240,3 +240,12 @@
           ;; (llm-ollama-host "10.27.27.100")
           (make-llm-ollama
            :chat-model "zephyr" :embedding-model "zephyr")))
+
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
--- packages.el
+++ packages.el
@@ -23,3 +23,6 @@
 (package! prettier)
 
 (package! ellama)
+
+(package! copilot
+  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
