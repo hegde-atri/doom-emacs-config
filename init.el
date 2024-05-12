27d3683bd9c04160416145f881d69d3107ddc1e7
Atri Hegde <iamatrihegde@outlook.com>
2024-05-12 13:31:54 +0100

    feat: packages

--- init.el
+++ init.el
@@ -86,9 +86,9 @@
 
        :tools
        ;;ansible
-       ;;biblio            ; Writes a PhD for you (citation needed)
-       ;;collab            ; buffers with friends
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       biblio            ; Writes a PhD for you (citation needed)
+       ;;(collab +tunnel)            ; buffers with friends
+       debugger          ; FIXME stepping through code, to help you add bugs
        direnv
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
