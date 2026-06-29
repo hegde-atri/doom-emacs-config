8ca9137363f251cb3294089a852d9e4f1046ab83
Atri Hegde <iamatrihegde@outlook.com>
2026-06-29 21:16:08 +0100

    feat(ui): refine display defaults

--- config.el
+++ config.el
@@ -19,6 +19,8 @@
 (map! :leader
       :desc "Save Buffer" "SPC" #'save-buffer)
 
+(setq-default tab-width 2)
+
 (setq doom-theme 'doom-palenight)
 
 ;; JetBrains Mono
@@ -28,18 +30,27 @@
 (setq doom-font (font-spec :family "Terminess Nerd Font Mono" :size 16 :weight 'medium)
       doom-variable-pitch-font (font-spec :family "Terminess Nerd Font Mono" :size 16 :weight 'medium))
 
-(doom-big-font-mode t)
+(when (display-graphic-p)
+  (doom-big-font-mode t))
 
 ;; Default global scroll margin
 (setq-default scroll-margin 7)
 
 ;; Apart from these modes which should have a scroll margin of 0
-(dolist (mode '(Info-mode term-mode eshell-mode shell-mode erc-mode vterm-mode))
+(dolist (mode '(Info-mode term-mode eshell-mode shell-mode erc-mode vterm-mode comint-mode))
   (add-hook (intern (format "%s-hook" mode))
             (lambda () (setq-local scroll-margin 0))))
 
 ;; Use relative line numbers
 (setq display-line-numbers-type 'relative)
+
+(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+(after! doom-modeline
+  (display-battery-mode t)
+  ;; Also have the file icon
+  (setq doom-modeline-major-mode-icon t))
 
 (setq org-directory "~/org/")
 
